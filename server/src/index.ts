0d4a3c6199cbc0f239fae15312d970e5f24d4b84
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-02-17 12:27:37 +0530

    database setup complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,25 @@
+import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
-const port = 3000;
+const mongoDBURL = process.env.MONGODB_URI || '';
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(mongoDBURL)
+  .then(() => {
+    console.log("MongoDB database connection established successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch(err => console.error("MongoDB connection error:", err));
 
 app.get('/', (_req, res) => {
-  res.send('Hello, TypeScript with Express!');
+  res.send('Hello!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
 
