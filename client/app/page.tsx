01b02381e1a2b3472f83da65cd3681476d637faa
not-wish <visheshagarwaltsk@gmail.com>
2026-03-05 20:14:33 +0530

    Added Poppins font, Color pallete to global.css Designed the Landing/ Hero Page from what 'Priyakshi Designed in Figma.

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,64 +2,59 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className="flex items-center justify-around min-h-screen bg-color1">
+        <div>
+          <div className="text-8xl font-poppins font-black">ELECTRON</div>
+          <div className="text-sm font-poppins font-extralight text-right">Plug into the future</div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className="flex flex-col justify-around gap-1">
+          <div>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              src="/heroimage1.png"
+              width={505}
+              height={250}
+              alt="Hero Image 1"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+          <div className="flex flex-row justify-around gap-1">
+            <Image
+              src="/heroimage2.png"
+              width={250}
+              height={250}
+              alt="Hero Image 2"
+            />
+            <Image
+              src="/heroimage3.png"
+              width={250}
+              height={250}
+              alt="Hero Image 3"
+            />
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+      {/* # Our Philosophy */}
+      <div className="flex items-center justify-around min-h-screen bg-color4">
+        <div className="flex flex-col justify-around min-h-fit gap-5 font-poppins text-black">
+          <div className="text-4xl font-bold">
+            Our Philosophy
+          </div>
+          <div className="text-md font-light">
+            Technology should be easy to find, not hard to discover.<br/>
+            At Electron, we believe searching for tech should feel as precise as writing a query.<br/>
+            Because when you know what you want, you shouldn't have to scroll through thousands of products to find it.<br/>
+            Just query it.
+          </div>
+        </div>
+      </div>
+      {/* # Shop All Categories */}
+      <div className="flex items-center justify-around min-h-screen bg-color5">
+        {/* We will use component: HeroPageCategoryCard to render all our categories 
+        I am adding dummy cards here rn*/}
+      </div>
+      {/* # Footer
+      note from Vishesh: soon to be converted to a component: Footer */}
+      <div className="flex items-center justify-around min-h-screen bg-color3"></div>
+    </>
   );
 }
