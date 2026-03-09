e534a35fa154b2b6631c3987915358b5af9b32aa
Anant Satya Mohit Kavuru <anant.kavuru@gmail.com>
2026-03-09 23:15:58 +0530

    port collisiion

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import productsRouter from './routes/products';
 
 const app = express();
 const mongoDBURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/electron-store';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
