1f4516e88fa27c8f697ab69d0c0f5c4c037cb9bc
priyakshisonowal555 <priyakshisonowal555@gmail.com>
2026-03-09 19:24:49 +0530

    Modfied collection/all page

--- client/app/collection/all/page.tsx
+++ client/app/collection/all/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div className='flex items-center justify-around min-h-screen bg-color2'>
-      <div>
+      <div className='text-white text-8xl font-poppins font-semibold'>
         All Collection
       </div>
     </div>
