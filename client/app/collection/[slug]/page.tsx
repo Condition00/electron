d96edada8a7417be3e2b389fdd1a94d19ac2b5b7
not-wish <visheshagarwaltsk@gmail.com>
2026-03-09 10:59:27 +0530

    Made the collections/[slug] page to render products from [slug] collection/ category. Also made a productCard ui, deleted the old productCard, added button and card from shadcn.

--- client/app/collection/[slug]/page.tsx
+++ client/app/collection/[slug]/page.tsx
@@ -1,12 +1,34 @@
+import ProductCard from "@/components/ProductCard";
+
 export default async function page({
     params,
 } : {
     params: Promise<{slug : string}>
 }) {
     const { slug } = await params
+    const products = [{
+        productId: 123,
+        productName: 'Samsung Galaxy Book',
+        category: 'Laptop',
+        price: 129000
+    },
+    {
+        productId: 124,
+        productName: 'HP Victus 15',
+        category: 'Laptop',
+        price: 69000
+    },]
+
     return(
-        <div className="font-poppins text-8xl text-white bg-black">
-            {slug}
+        <div>
+        <div className="flex items-center justify-around min-h-[40vh] font-opensans font-black text-8xl text-white bg-black">
+            {slug.toUpperCase()}
+        </div>
+        <div className="flex items-center justify-evenly gap-20 max-w=[100vw] flex-wrap px-4 py-10">
+            {products.map(p => 
+                <ProductCard key={p.productId} p={p}/>
+            )}
+        </div>
         </div>
     );
 }
