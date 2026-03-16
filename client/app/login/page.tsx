a61e30893272a852a0dd16d12808787adb64d28d
not-wish <visheshagarwaltsk@gmail.com>
2026-03-16 12:48:32 +0530

    Added shadcn : field, input, label, separator, Made a login page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,7 +1,37 @@
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
+} from "@/components/ui/field"
+import {Button} from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
 export default function page() {
     return(
-        <div className="">
-
+        <div className="flex items-center justify-around min-h-screen bg-color5">
+            <form className="w-full max-w-md bg-white border-2 border-white  rounded-md p-6">
+            <FieldSet >
+                <FieldGroup>
+                    <Field>
+                        <FieldLabel htmlFor="email">Email</FieldLabel>
+                        <Input id="email" type="email" placeholder="electron@gmail.com" />
+                        <FieldDescription>Enter your email address</FieldDescription>
+                    </Field>
+                    <Field>
+                        <FieldLabel htmlFor="password">Password</FieldLabel>
+                        <Input id="email" type="password" placeholder="*******" />
+                        <FieldDescription>Enter your password</FieldDescription>
+                    </Field>
+                    <Field>
+                        <Button type="submit">
+                            Login
+                        </Button>
+                    </Field>
+                </FieldGroup>
+            </FieldSet>
+            </form>
         </div>
     );
 }
